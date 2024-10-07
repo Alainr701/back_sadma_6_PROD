499919c6b67fcf65f189867cc0114d08e9534acd
Alain3127 <alain.ramos@fidooo.com>
2024-10-06 23:18:10 -0400

    login

--- src/models/auth.js
+++ src/models/auth.js
@@ -3,20 +3,11 @@ const db = require("./db");
 const User = {
   login: (user, password, callback) => {
     const query = `
-      SELECT 
-          id, 
-          nombres, 
-          apellidos, 
-          tipousuarios, 
-          ci, 
-          edad, 
-          \`user\`
-      FROM 
+      SELECT  * FROM 
           sadm6.usuarios
       WHERE 
-          \`user\` = '${user}' AND password = '${password}';
+          usuario = '${user}' AND password = '${password}';
     `;
-
     db.query(query, (err, results) => {
       if (err) {
         return callback(err, null);
